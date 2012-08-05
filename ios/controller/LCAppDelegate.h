d4f89a25f138c3b9f3bf271d07bfea8da1448079
Johannes Auer <johannesauer@me.com>
2012-08-05 20:18:59 +0200

    ios files added

---
+++ ios/controller/LCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LCAppDelegate.h
+//  Vault
+//
+//  Created by Johannes Auer on 05.08.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/misc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Vault
+//
+//  Created by Johannes Auer on 05.08.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([LCAppDelegate class]));
+  }
+}
