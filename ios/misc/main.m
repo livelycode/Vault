352b590cf04d609ed4b749a949f9eb95db8e55ee
Johannes Auer <johannes.auer@me.com>
2012-08-16 18:07:35 +0200

    ios project init

--- ios/misc/main.m
+++ ios/misc/main.m
@@ -1,18 +1,10 @@
-//
-//  main.m
-//  Vault
-//
-//  Created by Johannes Auer on 05.08.12.
-//
-//
 
 #import <UIKit/UIKit.h>
-
 #import "LCAppDelegate.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   @autoreleasepool {
-      return UIApplicationMain(argc, argv, nil, NSStringFromClass([LCAppDelegate class]));
+    NSString *className = NSStringFromClass([LCAppDelegate class]);
+    return UIApplicationMain(argc, argv, nil, className);
   }
 }
