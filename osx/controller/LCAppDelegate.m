57b0cc1f6698513644b29f13c9b7a1082f191b95
Mirko Kiefer <mail@mirkokiefer.com>
2012-08-18 13:42:31 +0200

    add icloud code

--- osx/controller/LCAppDelegate.m
+++ osx/controller/LCAppDelegate.m
@@ -13,6 +13,22 @@
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  id currentiCloudToken = [[NSFileManager defaultManager] ubiquityIdentityToken];
+  if (currentiCloudToken) {
+    NSData *newTokenData =
+    [NSKeyedArchiver archivedDataWithRootObject: currentiCloudToken];
+    [[NSUserDefaults standardUserDefaults]
+     setObject: newTokenData
+     forKey: @"com.apple.CloudNotes.UbiquityIdentityToken"];
+  } else {
+    [[NSUserDefaults standardUserDefaults]
+     removeObjectForKey: @"com.apple.CloudNotes.UbiquityIdentityToken"];
+  }
+  BOOL firstLaunchWithiCloudAvailable = YES;
+  if (currentiCloudToken && firstLaunchWithiCloudAvailable) {
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Choose Storage Option" defaultButton:@"iCloud" alternateButton:@"Local Only" otherButton:nil informativeTextWithFormat:@"Should documents be stored in iCloud and available on all your devices?"];
+    [alert runModal];
+  }
 }
 
 @end
