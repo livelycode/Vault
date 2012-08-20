787a933c515d1cd7fd1326142b0d08372aeab408
Mirko Kiefer <mail@mirkokiefer.com>
2012-08-20 22:44:45 +0200

    minor

--- common/LCUtils.h
+++ common/LCUtils.h
@@ -2,6 +2,8 @@
 #import <Foundation/Foundation.h>
 
 typedef void(^LCNotifyBlock)();
+typedef void(^LCBlock)();
+
 
 NSData *LCCreateSerializedPropertyList(id anObject);
 id LCCreateDeserializedPropertyList(NSData *data);
