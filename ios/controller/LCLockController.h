6d0689a790032ecfc966e8f4ea791ee987f0d97c
Johannes Auer <johannes.auer@me.com>
2012-09-09 14:21:00 +0200

    lock view draws with gradient and grain

--- ios/controller/LCLockController.h
+++ ios/controller/LCLockController.h
@@ -1,5 +1,6 @@
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
 #import "LCAppDelegate.h"
 
 @interface LCLockController : UIViewController <UITextFieldDelegate> @end
--- ios/controller/LCLockController.m
+++ ios/controller/LCLockController.m
@@ -3,6 +3,7 @@
 #import "LCSettingsStore.h"
 
 @implementation LCLockController
+#pragma mark - UITextFieldDelegate
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
   LCEncryptedData *encryptedPassword = [LCSettingsStore sharedSettingsStore].encryptedPassword;
   if ([encryptedPassword isEqualToPassword:textField.text]) {
@@ -12,5 +13,30 @@
     [[[UIAlertView alloc] initWithTitle:@"foo" message:@"bar" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
   }
   return NO;
+}
+
+#pragma mark - Overridden
+- (void)viewDidLoad {
+  CALayer *layer = self.view.layer;
+  [layer insertSublayer:LCGradient(layer.bounds) atIndex:0];
+  [layer insertSublayer:LCGrain(layer.bounds) atIndex:1];
+}
+
+#pragma mark - Private
+CAGradientLayer *LCGradient(CGRect frame) {
+  UIColor *topColor = [UIColor colorWithWhite:(220.0/255.0) alpha:1.0];
+  UIColor *bottomColor = [UIColor colorWithWhite:(140.0/255.0) alpha:1.0];
+  CAGradientLayer *layer = [CAGradientLayer layer];
+  layer.frame = frame;
+  layer.colors = @[(id)topColor.CGColor, (id)bottomColor.CGColor];
+  return layer;
+}
+
+CALayer *LCGrain(CGRect frame) {
+  CALayer *layer = [CALayer layer];
+  layer.frame = frame;
+  layer.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"Grain"]].CGColor;
+  layer.opacity = 8.0/255.0;
+  return layer;
 }
 @end
